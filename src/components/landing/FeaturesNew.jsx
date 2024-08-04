f8409175206fcfdbed4de3f90963cb8cb2fe125c
AhmedCoolProjects <bargadyahmed2000@gmail.com>
2024-08-04 18:26:25 +0100

    features in landing page separated to different sections with animation one time

---
+++ src/components/landing/FeaturesNew.jsx
@@ -0,0 +1,143 @@
+import * as React from "react";
+import { motion, useAnimation } from "framer-motion";
+import clsx from "clsx";
+import Wrapper from "../global/Wrapper";
+// import { useInView } from "react-intersection-observer";
+
+const FEATURES = [
+  {
+    title: "Fully private",
+    imageUrl: "/img/undraw_docusaurus_react.svg",
+    description: (
+      <>
+        No one wants to learn their data was used for AI training or part of a
+        multi-tenant breach. All services are run privately in <em>your</em> AWS
+        account. Your data{" "}
+        <strong className="text-primary">always remains yours</strong>,{" "}
+        <strong className="text-primary">only accessible by you</strong>.
+      </>
+    ),
+    target: "",
+  },
+  {
+    title: "Unlimited Everything",
+    imageUrl: "/img/undraw_docusaurus_tree.svg",
+    description: (
+      <>
+        Design and incorporate{" "}
+        <strong className="text-primary">real-time messaging</strong> the way
+        that makes the most sense for your applications with{" "}
+        <strong className="text-primary">unlimited</strong> connections,
+        channels, and messages.
+      </>
+    ),
+    target: "",
+  },
+  {
+    title: "Serverless",
+    imageUrl: "/img/undraw_docusaurus_mountain.svg",
+    description: (
+      <>
+        Powered by on-demand services such as{" "}
+        <strong className="text-primary">API Gateway</strong>,{" "}
+        <strong className="text-primary">DynamoDB</strong>,{" "}
+        <strong className="text-primary">Lambda</strong>,{" "}
+        <strong className="text-primary">EventBridge</strong>, and{" "}
+        <strong className="text-primary">SQS</strong>. It scales to zero when
+        you're not using it or can handle millions of connections and billions
+        of messages for high-volume production workloads.
+      </>
+    ),
+    target: "",
+  },
+];
+
+function NewFeatures() {
+  return (
+    <Wrapper>
+      <div className=" space-y-8 my-12">
+        <div className="text-center">
+          <h1 className="text-3xl m-0  text-center lg:text-4xl">Features</h1>
+          <p className="mt-4 text-xl mb-8 ">
+            What makes us different from others
+          </p>
+        </div>
+        {FEATURES.map((feature, idx) => (
+          <FeatureAnimation key={idx} idx={idx} feature={feature} />
+        ))}
+      </div>
+    </Wrapper>
+  );
+}
+
+function FeatureAnimation({ idx, feature }) {
+  return (
+    <motion.div
+      className={clsx(
+        "flex  items-center space-x-0 md:space-x-8",
+        idx % 2 !== 0 ? "flex-col md:flex-row" : "flex-col md:flex-row-reverse"
+      )}
+      key={idx}
+      initial={{ opacity: 0, x: idx % 2 === 0 ? -100 : 100 }}
+      animate={{ opacity: 1, x: 0 }}
+      transition={{ duration: 0.5, delay: idx * 0.1 }}
+    >
+      <DescriptionSection
+        title={feature.title}
+        description={feature.description}
+        target={feature.target}
+        idx={idx}
+      />
+      <ImageSection src={feature.imageUrl} idx={idx} />
+    </motion.div>
+  );
+}
+
+function DescriptionSection({ title, description, target, idx }) {
+  //   const { ref, = useInView();
+
+  return (
+    <motion.div
+      className="flex py-12 min-h-[400px] flex-1 flex-col items-start justify-start"
+      initial={{ opacity: 0, y: 20 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.5, delay: idx * 0.1 + 0.3 }}
+    >
+      <h1 className="text-3xl">{title}</h1>
+      <motion.p
+        className="text-xl"
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5, delay: idx * 0.1 + 0.5 }}
+      >
+        {description}
+      </motion.p>
+      <motion.a
+        href={target}
+        className="text-cusprimary underline underline-offset-4"
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5, delay: idx * 0.1 + 0.7 }}
+      >
+        Learn More
+      </motion.a>
+    </motion.div>
+  );
+}
+
+function ImageSection({ src, idx }) {
+  return (
+    <div className="flex flex-1 flex-col items-center justify-center">
+      <motion.img
+        className="w-2/3 object-contain"
+        src={src}
+        alt={src}
+        initial={{ opacity: 0, x: idx % 2 === 0 ? -100 : 100 }}
+        animate={{ opacity: 1, x: 0 }}
+        transition={{ duration: 0.5 }}
+      />
+    </div>
+  );
+}
+
+export default NewFeatures;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,60 +1,10 @@
-import clsx from "clsx";
-import Link from "@docusaurus/Link";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import Layout from "@theme/Layout";
 import HomepageBanner from "@site/src/components/HomepageBanner";
-import HomepageFeatures from "@site/src/components/HomepageFeatures";
-import HomepageInstall from "@site/src/components/HomepageInstall";
-import HomepagePublish from "@site/src/components/HomepagePublish";
-import HomepageSubscribe from "@site/src/components/HomepageSubscribe";
-
-import styles from "./index.module.css";
 import Hero from "../components/landing/Hero";
-import TabsSection from "../components/landing/TabsSection";
 import Sections from "../components/landing/Sections";
-import Features from "../components/landing/Features";
 import Testimonials from "../components/landing/Testimonials";
-
-function HomepageHeroImage({ name }) {
-  const { siteConfig } = useDocusaurusContext();
-  return (
-    <header className={clsx("hero hero--image", styles[name])}>
-      <div className="container"></div>
-    </header>
-  );
-}
-
-function HomepageHeader() {
-  const { siteConfig } = useDocusaurusContext();
-  return (
-    <header className={clsx("hero", styles.heroBanner)}>
-      <div className="container">
-        <h1 className={clsx("hero__title", styles.heroTitle)}></h1>
-        <p className={clsx("hero__subtitle", styles.heroSubtitle)}></p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/installation/initial-setup"
-          >
-            Install yourself in less than 20 mins ⏱️
-          </Link>
-        </div>
-      </div>
-    </header>
-  );
-}
-
-// export default function Home() {
-//   return (
-//     <Layout>
-//       <main>
-//         <div className="container mt-8">
-//           <h1>Hotsock launches August 12, 2024, check back then! 🚀</h1>
-//         </div>
-//       </main>
-//     </Layout>
-//   )
-// }
+import FeaturesNew from "../components/landing/FeaturesNew";
 
 export default function Home() {
   const { siteConfig } = useDocusaurusContext();
@@ -68,10 +18,9 @@ export default function Home() {
         <Hero />
         <Sections />
         {/* features */}
-        <Features />
+        <FeaturesNew />
         {/* testimonials */}
         <Testimonials />
-        {/* footer */}
       </Layout>
     </>
   );
