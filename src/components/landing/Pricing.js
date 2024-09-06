9f3b4fc1abe0040e7a5603a29804ee48d81269b6
James Miller <bensie@gmail.com>
2024-09-06 08:54:17 -0700

    reduce confusion

--- src/components/landing/Pricing.js
+++ src/components/landing/Pricing.js
@@ -24,13 +24,11 @@ function PricingTable() {
           <div className="w-full bg-pink-50/70 dark:bg-gray-800  flex col-span-2   flex-col items-center text-center h-fit  ">
             <div className="table-title">Price</div>
             <div className="table-item">$1.25 per million messages</div>
-            <div className="table-item">$99/month or $1,069/year</div>
-            <div className="table-item">$299/month or $3,229/year</div>
-            <div className="table-item">$599/month or $6,469/year</div>
-            <div className="table-item">$1,299/month or $14,029/year</div>
-            <div className="table-item  !border-b-0 ">
-              $2,799/month or $30,229/year
-            </div>
+            <div className="table-item">$99/month</div>
+            <div className="table-item">$299/month</div>
+            <div className="table-item">$599/month</div>
+            <div className="table-item">$1,299/month</div>
+            <div className="table-item !border-b-0">$2,799/month</div>
           </div>
           {/* second column */}
           <div className="w-full flex col-span-4 max-md:col-span-3 border-l border-solid border-y-0 border-gray-400 border-r flex-col items-center text-center min-h-fit ">
@@ -113,102 +111,3 @@ function PricingTable() {
 }
 
 export default PricingTable
-
-// old table with splitted columns
-// function PricingTable() {
-//   return (
-//     <Wrapper className="">
-//       <h1 className="text-center w-full py-4 mt-4">Pricing Plans</h1>
-//       <div className="w-full max-w-full overflow-x-auto">
-//         <main className="grid  w-full max-md:w-[150vw]  py-4 gap-3 lg:gap-4 grid-cols-12">
-//           {/* first column */}
-//           <div className="w-full bg-pink-50/70 dark:bg-gray-800  flex col-span-2  flex-col items-center text-center h-fit  ">
-//             <div className="table-title">Price</div>
-//             <div className="sub-table !border-none"></div>
-//             <div className="table-item">$1.25 per million messages</div>
-//             <div className="table-item">$99/month or $1,069/year</div>
-//             <div className="table-item">$299/month or $3,229/year</div>
-//             <div className="table-item">$599/month or $6,469/year</div>
-//             <div className="table-item">$1,299/month or $14,029/year</div>
-//             <div className="table-item">$2,799/month or $30,229/year</div>
-//           </div>
-//           {/* second column */}
-//           <div className="w-full flex col-span-4  flex-col items-center text-center min-h-fit ">
-//             <div className="table-title">Included messages per month</div>
-//             <div className="sub-table">
-//               <span>first</span>
-//               <span>then</span>
-//             </div>
-//             <div className="sub-table !font-normal !h-24">
-//               <span>1 million free messages each month</span>
-//               <span>pay as you go</span>
-//             </div>
-//             <div className="sub-table !font-normal !h-24">
-//               <span>300 million (~10 million per day)</span>
-//               <span>$0.50 per additional million</span>
-//             </div>
-//             <div className="sub-table !font-normal !h-24">
-//               <span>1.5 billion (~50 million per day)</span>
-//               <span>$0.45 per additional million</span>
-//             </div>
-//             <div className="sub-table !font-normal !h-24">
-//               <span>7.5 billion (~250 million per day)</span>
-//               <span>$0.40 per additional million</span>
-//             </div>
-//             <div className="sub-table !font-normal !h-24">
-//               <span>30 billion (~1 billion per day)</span>
-//               <span>$0.35 per additional million</span>
-//             </div>
-//             <div className="sub-table !font-normal !h-24">
-//               <span>150 billion (~5 billion per day)</span>
-//               <span>$0.30 per additional million</span>
-//             </div>
-//           </div>
-//           {/* third column */}
-//           <div className="w-full bg-pink-50/70 dark:bg-gray-800 flex col-span-4  flex-col items-center text-center min-h-fit ">
-//             <div className="table-title">AWS Configuration</div>
-//             <div className="sub-table">
-//               <span>Accounts</span>
-//               <span>Regions</span>
-//             </div>
-//             <div className="sub-table !font-normal !h-24">
-//               <span>1</span>
-//               <span>1</span>
-//             </div>
-//             <div className="sub-table !font-normal !h-24">
-//               <span>1</span>
-//               <span>1</span>
-//             </div>
-//             <div className="sub-table !font-normal !h-24">
-//               <span>5</span>
-//               <span>2</span>
-//             </div>
-//             <div className="sub-table !font-normal !h-24">
-//               <span>25</span>
-//               <span>5</span>
-//             </div>
-//             <div className="sub-table !font-normal !h-24">
-//               <span>100</span>
-//               <span>All regions</span>
-//             </div>
-//             <div className="sub-table !font-normal !h-24">
-//               <span>500</span>
-//               <span>All regions</span>
-//             </div>
-//           </div>
-//           {/* fourth */}
-//           <div className="w-full flex col-span-2  flex-col items-center text-center h-fit  ">
-//             <div className="table-title">Support</div>
-//             <div className="sub-table !border-none"></div>
-//             <div className="table-item !font-normal">Standard</div>
-//             <div className="table-item !font-normal">Standard</div>
-//             <div className="table-item !font-normal">Priority</div>
-//             <div className="table-item !font-normal">Priority</div>
-//             <div className="table-item !font-normal">Priority</div>
-//             <div className="table-item !font-normal">Priority</div>
-//           </div>
-//         </main>
-//       </div>
-//     </Wrapper>
-//   );
-// }
