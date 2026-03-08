8126d553244c993716c0cd0b6fa005c18b6d065f
James Miller <bensie@gmail.com>
2026-03-08 14:44:01 -0700

    Move onBrokenMarkdownLinks to markdown.hooks per Docusaurus v4 deprecation

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -19,8 +19,12 @@ const config = {
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
   onBrokenLinks: "throw",
-  onBrokenMarkdownLinks: "warn",
   trailingSlash: true,
+  markdown: {
+    hooks: {
+      onBrokenMarkdownLinks: "warn",
+    },
+  },
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
